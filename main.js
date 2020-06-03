ceff73ab2c98b8b2a4edbc54051add6ef6fb2b8b
stefan hranek <hranekstefan@gmail.com>
2020-06-03 10:08:14 -1100

    Pluto can be removed from dropdown via checkbox

--- main.js
+++ main.js
@@ -38,9 +38,25 @@ const handleClickEvent = () => {
   let userWeight = document.getElementById("user-weight").value;
   let planetName = document.getElementById("planets").value;
   let result = calculateWeight(userWeight, planetName);
-  document.getElementById("output").innerHTML = `If you were on <span class="text-accent-color">${planetName}</span>, you would weigh <span class="text-accent-color">${result}lbs</span>!`;
+  document.getElementById(
+    "output"
+  ).innerHTML = `If you were on <span class="text-accent-color">${planetName}</span>, you would weigh <span class="text-accent-color">${result}lbs</span>!`;
 };
 
 // Add a checkbox to the application that when checked will remove Pluto as an option in the dropdown list.
+const handleCheckbox = () => {
+  let checkbox = document.querySelector(".checkbox").value;
+  let planets = document.getElementById("planets");
+  if (checkbox === "no") {
+    document.querySelector(".checkbox").value = "yes";
+    for (var i = 0; i < planets.length; i++) {
+      if (planets.options[i].value == "Pluto") planets.remove(i);
+    }
+    console.log("fuck pluto");
+  } else {
+    document.querySelector(".checkbox").value = "no";
+    console.log("i tihink pluto is a plent");
+  }
+};
 
 // Add a custom planet with its multiplier to the dropdown. Add two input fields (one for name, one for multiplier) and a button that once clicked will push a new planet to the dropdown.
